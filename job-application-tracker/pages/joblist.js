8624a1ab45e3202d7e95023f18928a839e53be91
Linda <joling6027@gmail.com>
2022-12-28 13:49:44 -0800

    display from db

--- job-application-tracker/pages/joblist.js
+++ job-application-tracker/pages/joblist.js
@@ -1,11 +1,41 @@
 import JobListTable from "../components/table";
+import connectMongodb from "../utility/connectMongodb";
+import JobApplied from "../models/jobModel";
 
-const JobList = () => {
+export const getServerSideProps = async () => {
+  try {
+    console.log('CONNECTION TO MONGO');
+
+    await connectMongodb();
+    console.log('CONNECTED TO MONGODB');
+
+    console.log('FETCHING DOCUMENT');
+    const jobs = await JobApplied.find();
+
+    console.log('DOCUMENT FETCHED');
+
+    return {
+      props: {
+        jobs: JSON.parse(JSON.stringify(jobs))
+      },
+    };
+
+  } catch (err) {
+    res.json({ err });
+    return {
+      notFound: true,
+    };
+  }
+
+}
+
+const JobList = ({jobs}) => {
+
   return ( 
     <>
       <h1>Job Application List</h1>
       <p>Here shows the job you have already applied</p>
-      <JobListTable />
+      <JobListTable jobs={jobs}/>
     </>
    );
 }
